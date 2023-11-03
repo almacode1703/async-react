14beb5fed5bd7a1c535c5f3093e970fb349ca068
almacode1703 <testmygitt.2023@gmail.com>
2023-11-04 05:25:42 +0530

    Search functionality implemented

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import FetchData from "./components/FetchData";
 import FetchAsyncData from './components/FetchAsyncData'
+import SimpleSearch from "./components/SimpleSearch";
 
 function App() {
   return (
     <div className="App">
        <h1>Fetching Data from API</h1>
         {/*<FetchData/>*/}
-        <FetchAsyncData/>
+        {/*<FetchAsyncData/>*/}
+        <SimpleSearch/>
     </div>
   );
 }
